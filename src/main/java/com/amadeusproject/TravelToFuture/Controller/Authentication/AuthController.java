9bdf9750156d3ad04ab2f87faa793fa95341d2e6
ilkercor <ilker.cor19@gmail.com>
2023-08-20 06:41:49 +0300

    Jwt Authentication added.

--- src/main/java/com/amadeusproject/TravelToFuture/Controller/Authentication/AuthController.java
+++ src/main/java/com/amadeusproject/TravelToFuture/Controller/Authentication/AuthController.java
@@ -1,11 +1,14 @@
 package com.amadeusproject.TravelToFuture.Controller.Authentication;
 
+import com.amadeusproject.TravelToFuture.DTO.AuthResponseDTO;
 import com.amadeusproject.TravelToFuture.DTO.LoginDTO;
 import com.amadeusproject.TravelToFuture.DTO.RegisterDTO;
 import com.amadeusproject.TravelToFuture.DataAccess.Role.RoleRepository;
 import com.amadeusproject.TravelToFuture.DataAccess.User.UserRepository;
 import com.amadeusproject.TravelToFuture.Entities.Role;
 import com.amadeusproject.TravelToFuture.Entities.UserEntity;
+import com.amadeusproject.TravelToFuture.Service.JWT.JwtService;
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -30,26 +33,33 @@ public class AuthController {
     private UserRepository userRepository;
     private RoleRepository roleRepository;
     private PasswordEncoder passwordEncoder;
+    private JwtService jwtService;
 
 
     @Autowired
-    public AuthController(AuthenticationManager authenticationManager, UserRepository userRepository, RoleRepository roleRepository, PasswordEncoder passwordEncoder) {
+    public AuthController(AuthenticationManager authenticationManager, UserRepository userRepository,
+                          RoleRepository roleRepository, PasswordEncoder passwordEncoder,
+                          JwtService jwtService) {
         this.authenticationManager = authenticationManager;
         this.userRepository = userRepository;
         this.roleRepository = roleRepository;
         this.passwordEncoder = passwordEncoder;
+        this.jwtService = jwtService;
     }
 
+    @Operation(summary = "Login with username and password.")
     @PostMapping("/login")
-    public ResponseEntity<String> login(@RequestBody LoginDTO loginDTO){
+    public ResponseEntity<AuthResponseDTO> login(@RequestBody LoginDTO loginDTO){
         Authentication authentication = authenticationManager.authenticate(
                 new UsernamePasswordAuthenticationToken(loginDTO.getUsername(),
                 loginDTO.getPassword()));
         SecurityContextHolder.getContext().setAuthentication(authentication);
-        return new ResponseEntity<>("User signed success",HttpStatus.OK);
+        String token = jwtService.generateToken(authentication);
+        return new ResponseEntity<>(new AuthResponseDTO(token),HttpStatus.OK);
     }
 
 
+    @Operation(summary = "Register with username and password")
     @PostMapping("/register")
     public ResponseEntity<String> register(@RequestBody RegisterDTO registerDTO){
         if(userRepository.existsByUsername(registerDTO.getUsername())){
@@ -59,8 +69,13 @@ public class AuthController {
         UserEntity user = new UserEntity();
         user.setUsername(registerDTO.getUsername());
         user.setPassword(passwordEncoder.encode((registerDTO.getPassword())));
+        Role roles;
+        if(registerDTO.getUsername().endsWith("@admin")){
 
-        Role roles = roleRepository.findByName("USER").get();
+            roles = roleRepository.findByName("ADMIN").get();
+        }else {
+            roles = roleRepository.findByName("USER").get();
+        }
         user.setRoles(Collections.singletonList(roles));
 
         userRepository.save(user);
--- src/main/java/com/amadeusproject/TravelToFuture/Security/SecurityConfig.java
+++ src/main/java/com/amadeusproject/TravelToFuture/Security/SecurityConfig.java
@@ -9,13 +9,14 @@ import org.springframework.security.config.annotation.authentication.configurati
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -23,25 +24,34 @@ public class SecurityConfig {
 
     private CustomUserDetailsService userDetailsService;
 
+    private JwtAuthEntryPoint jwtAuthEntryPoint;
+
     @Autowired
-    public SecurityConfig(CustomUserDetailsService userDetailsService) {
+    public SecurityConfig(CustomUserDetailsService userDetailsService, JwtAuthEntryPoint jwtAuthEntryPoint) {
         this.userDetailsService = userDetailsService;
+        this.jwtAuthEntryPoint = jwtAuthEntryPoint;
     }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
         http
                 .csrf().disable()
-                .authorizeHttpRequests()
+                .exceptionHandling()
+                .authenticationEntryPoint(jwtAuthEntryPoint)
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeRequests()
                 .requestMatchers("/api/v1/auth/**",
                         "/swagger-ui/**",
                         "/v3/api-docs/**",
-                        "/auth/**",
-                        "/airports/**",
-                        "/flights/**")
+                        "/auth/**")
                 .permitAll()
                 .anyRequest()
                 .authenticated();
+
+        http.addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
@@ -56,4 +66,10 @@ public class SecurityConfig {
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
+
+    @Bean
+    public JWTAuthenticationFilter jwtAuthenticationFilter(){
+        return new JWTAuthenticationFilter();
+    }
+
 }
